a78d3a86a43140c6a85aabe8760c220a5611818d
pmh <peter.haubrick@cambridgeconsultants.com>
2020-10-20 09:56:45 +0100

    First blank window

--- frontend.py
+++ frontend.py
@@ -1,7 +1,18 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MediaBrowserWidget(Widget):
+    pass
+
+
+class MediaBrowserApp(App):
+    def build(self):
+        return MediaBrowserWidget()
 
 
 def main():
-    print('Started')
+    MediaBrowserApp().run()
 
 
 if __name__ == "__main__":
